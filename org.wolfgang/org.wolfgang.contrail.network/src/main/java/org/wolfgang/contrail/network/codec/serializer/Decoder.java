b403b5ec9cbd244845976b09eaad24761f4efd81
Didier Plaindoux <d.plaindoux@free.fr>
2012-03-22 07:17:18 +0100

    Refactoring
    Remove useless classes

--- org.wolfgang/org.wolfgang.contrail.network/src/main/java/org/wolfgang/contrail/network/codec/serializer/Decoder.java
+++ org.wolfgang/org.wolfgang.contrail.network/src/main/java/org/wolfgang/contrail/network/codec/serializer/Decoder.java
@@ -19,100 +19,42 @@
 package org.wolfgang.contrail.network.codec.serializer;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.wolfgang.common.utils.Marshall;
-import org.wolfgang.common.utils.Option;
 import org.wolfgang.contrail.component.transducer.DataTransducer;
 import org.wolfgang.contrail.component.transducer.DataTransducerException;
+import org.wolfgang.contrail.network.codec.payload.Bytes;
 
 /**
- * <code>Decoder</code> is able to transform payload based array to objects.
+ * <code>Decoder</code> is able to transform payload based array to an object
  * 
  * @author Didier Plaindoux
  * @version 1.0
  */
-class Decoder implements DataTransducer<byte[], Object> {
-
-	/**
-	 * Static definition for int len in byte stream representation (32 bits)
-	 */
-	private static final int INT_LEN = 4;
-
-	/**
-	 * The empty buffer
-	 */
-	private static final byte[] EMPTY_BUFFER = new byte[0];
-
-	/**
-	 * The decoding buffer used for the object de-serialization
-	 */
-	private byte[] buffer;
+class Decoder implements DataTransducer<Bytes, Object> {
 
 	/**
 	 * Constructor
 	 */
 	Decoder() {
 		super();
-		this.buffer = EMPTY_BUFFER;
-	}
-
-	/**
-	 * Method in charge of object decoding if possible. The monadic result
-	 * reflects the system capability to decode or not an object from the source
-	 * 
-	 * @return an optional object
-	 * @throws IOException
-	 *             Thrown if the decoding stage fails while it reads data from
-	 *             the source
-	 * @throws ClassNotFoundException
-	 *             Thrown if the required class is not know
-	 */
-	private Option<Object> getNext() throws IOException, ClassNotFoundException {
-		if (buffer.length < INT_LEN) {
-			return Option.none();
-		} else {
-			// Read the coded object payload first
-			final int payLoad = Marshall.bytesToInt(buffer);
-			if (buffer.length - INT_LEN < payLoad) {
-				return Option.none();
-			} else {
-				final byte[] array = Arrays.copyOfRange(buffer, INT_LEN, payLoad + INT_LEN);
-				buffer = Arrays.copyOfRange(buffer, payLoad + INT_LEN, buffer.length);
-				return Option.some(Marshall.bytesToObject(array));
-			}
-		}
 	}
 
 	@Override
-	public List<Object> transform(byte[] source) throws DataTransducerException {
+	public List<Object> transform(Bytes source) throws DataTransducerException {
 		try {
-			// First concatenate remaining buffer with the new one
-			final byte[] newBuffer = new byte[buffer.length + source.length];
-			System.arraycopy(buffer, 0, newBuffer, 0, buffer.length);
-			System.arraycopy(source, 0, newBuffer, buffer.length, source.length);
-			buffer = newBuffer;
-
-			// Try to decode objects
-			final List<Object> objects = new ArrayList<Object>();
-			for (Option<Object> option = getNext(); option.haveSome(); option = getNext()) {
-				objects.add(option.getValue());
-			}
-
-			return objects;
-		} catch (Exception e) {
+			return Arrays.asList(Marshall.bytesToObject(source.getContent()));
+		} catch (IOException e) {
+			throw new DataTransducerException(e);
+		} catch (ClassNotFoundException e) {
 			throw new DataTransducerException(e);
 		}
 	}
 
 	@Override
 	public List<Object> finish() throws DataTransducerException {
-		if (buffer.length > 0) {
-			throw new DataTransducerException();
-		} else {
-			return Arrays.asList();
-		}
+		return Arrays.asList();
 	}
 }
