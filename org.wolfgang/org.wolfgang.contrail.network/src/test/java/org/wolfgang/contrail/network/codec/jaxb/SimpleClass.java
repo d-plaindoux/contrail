3c8f06136480dc5bc83fc4b9ccf551e6c4a9c8de
Didier Plaindoux <d.plaindoux@free.fr>
2012-03-22 08:39:36 +0100

    Test
    JAXB transducer tests

---
+++ org.wolfgang/org.wolfgang.contrail.network/src/test/java/org/wolfgang/contrail/network/codec/jaxb/SimpleClass.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C)2012 D. Plaindoux.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+package org.wolfgang.contrail.network.codec.jaxb;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * <code>SimpleClass</code>
+ * 
+ * @author Didier Plaindoux
+ * @version 1.0
+ */
+@XmlRootElement
+public class SimpleClass {
+	private String value;
+
+	public SimpleClass() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	@XmlElement
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof SimpleClass)) {
+			return false;
+		}
+		SimpleClass other = (SimpleClass) obj;
+		if (value == null) {
+			if (other.value != null) {
+				return false;
+			}
+		} else if (!value.equals(other.value)) {
+			return false;
+		}
+		return true;
+	}
+}
