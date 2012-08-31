4c64ae682c57edf4ef5e0761efa5cb85d38ca3b1
Didier Plaindoux <d.plaindoux@free.fr>
2012-08-31 21:15:59 +0200

    Refactoring

--- org.wolfgang/org.wolfgang.contrail-core/src/main/java/org/wolfgang/contrail/connection/CannotCreateClientException.java
+++ org.wolfgang/org.wolfgang.contrail-core/src/main/java/org/wolfgang/contrail/connection/CannotCreateClientException.java
@@ -38,7 +38,6 @@ public class CannotCreateClientException extends Exception {
 	 */
 	public CannotCreateClientException(Throwable arg0) {
 		super(arg0);
-		// TODO Auto-generated constructor stub
 	}
 
 }
